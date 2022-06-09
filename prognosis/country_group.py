792f482da4e99b2fc0394bfbde858bc8e1257c81
xspirit <48792113+georgg2003@users.noreply.github.com>
2022-06-09 18:53:17 +0300

    country_group by string name

--- prognosis/country_group.py
+++ prognosis/country_group.py
@@ -7,7 +7,7 @@ Created on Fri May 20 16:07:36 2022
 
 import pandas as pd
 from prognosis.helper import get
-from prognosis.com import topic_tickers, topic_df
+from prognosis.com import topic_tickers, topic_df, country_groups, country_df
 
 
 topics = topic_df['code'].tolist()
@@ -15,7 +15,15 @@ topics = topic_df['code'].tolist()
 
 class CountryGroup():
     def __init__(self, country_list):
-        self.country_list = country_list
+        if isinstance(country_list, str):
+            self.country_list = country_groups[country_list]
+        else:
+            assert isinstance(country_list, list)
+            countries = country_df["iso2"].values()
+            for country in country_list:
+                if not country in countries:
+                    raise Exception("%s does not exist" % country)
+            self.country_list = country_list
 
     def get_topic(self, name):
         assert name in topics, 'Choose among %s' % str(topics)
