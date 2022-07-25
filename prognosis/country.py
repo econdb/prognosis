cd9f645815e0b11b59fb776a0ffa89cdb320e76d
Econdb <admin@econdb.com>
2022-07-25 23:08:09 +0100

    .

--- prognosis/country.py
+++ prognosis/country.py
@@ -14,11 +14,14 @@ class Country():
     def __init__(self, country_code):
         self.country_code = country_code
 
-    def get_group(self, name):
-        codes = [x % self.country_code for x in topic_tickers[name]]
+    def get_group(self, name, **k):
+
+        codes = [(x % self.country_code) +
+                 (('~T%s' % k.get('transform')) if k.get('transform') else '')
+                 for x in topic_tickers[name]]
         return get(codes)
 
-    def national_accounts(self):
+    def national_accounts(self, **k):
         """
         RGDP: Real gross domestic product
         RPRC: Real private consumption
@@ -29,27 +32,27 @@ class Country():
 
         Note: Real refers to inflation adjusted indicators
         """
-        return self.get_group('national_accounts')
+        return self.get_group('national_accounts', **k)
 
-    def prices(self):
-        return self.get_group('prices')
+    def prices(self, **k):
+        return self.get_group('prices', **k)
 
-    def monthly_trade(self):
-        return self.get_group('monthly_trade')
+    def monthly_trade(self, **k):
+        return self.get_group('monthly_trade', **k)
 
-    def government_accounts(self):
-        return self.get_group('government')
+    def government_accounts(self, **k):
+        return self.get_group('government', **k)
 
-    def yield_curve(self):
-        return self.get_group('yield_curve')
+    def yield_curve(self, **k):
+        return self.get_group('yield_curve', **k)
 
-    def retail_sales(self):
-        return self.get_group('retail_sales')
+    def retail_sales(self, **k):
+        return self.get_group('retail_sales', **k)
 
-    def ip(self):
-        return self.get_group('ip')
+    def ip(self, **k):
+        return self.get_group('ip', **k)
 
-    def energy(self):
+    def energy(self, **k):
         """
         OILPROD: Oil production, thousands barrels per day
         OILDEM: Oil demand, thousands barrels per day
@@ -58,4 +61,4 @@ class Country():
         GASDEM: Gas demand, millions of cubic meters per month
         GASPROD: Gas production, millions of cubic meters per month
         """
-        return self.get_group('energy')
+        return self.get_group('energy', **k)
